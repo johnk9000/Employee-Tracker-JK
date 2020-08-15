7087da53d49243610f324d63fdc07fb2668bd280
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2020-08-15 03:01:05 -0500

    Should deploy properly on Heroku now

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, 'employee-tracker', 'public');
+const publicPath = path.join(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
