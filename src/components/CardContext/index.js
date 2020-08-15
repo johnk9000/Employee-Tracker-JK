5a2d66877e75dbd9398bfcf886e943d548cc50f7
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2020-08-15 04:54:36 -0500

    Removing directories of stuff

--- src/components/CardContext/index.js
+++
@@ -1,9 +0,0 @@
-import React, {useContext} from "react"
-import FormContext from "../FormContext/"
-
-function EmployeeDetails() {
-    const { homie } = useContext(FormContext)
-    return { ...homie }
-}
-
-export default EmployeeDetails;
