d3554b7f32b85af7a618d05a65a5edaeb39b8a2b
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2020-08-24 01:10:01 -0500

    Should be deployable now

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
 
-//  app.get('/*', function (req, res) {
-//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//  });
-
 app.listen(port, () => {
    console.log(`Server is up on port ${port}!`);
 });
